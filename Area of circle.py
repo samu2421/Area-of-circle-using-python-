e8dd8b47bf49d6a0fed26d8b54eb05ce3a985e34
samu2421 <95489339+samu2421@users.noreply.github.com>
2022-03-04 16:31:17 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,10 @@
+#area of circle
+
+from math import pi
+
+print("Enter the value of Radius")
+r = float(input())
+
+print("Entered radius of circle is : ", r)
+
+print("Area of circle with radius", r ,"is", pi * r**2)
